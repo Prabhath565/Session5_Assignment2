b743de3def949b224082c5dd0cd0d34bb32776b9
Prabhath565 <prabhath565@gmail.com>
2016-12-14 22:20:17 +0530

    S5_2

---
+++ external.js
@@ -0,0 +1,13 @@
+		var empty_object = {};
+
+		var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+		var stooge = {
+			"first-name": "Jerome",
+			"last-name": "Howard"
+		};
+		
+		console.log(empty_object);
+		console.log(person);
+		console.log(stooge);
+
